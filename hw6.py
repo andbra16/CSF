2cff35f01a0527cc96cc8af7a971c13c081bf611
andbra16 <andbra16@ccm-2610-d4.evergreen.edu>
2013-11-14 18:01:27 -0800

    fadfasfa

--- hw6.py
+++ hw6.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Brandon Anderson
+# Evergreen Login: andbra16
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 6
@@ -17,8 +17,14 @@
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 3 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+a=2
+b=3
+#assert a==b
 
+# the assert statement makes a boolean statement:
+# if your assertion is true, the program runs.
+# if your assertion is false, the program gets an
+# assertion error.
 
 ###
 ### Problem 4
@@ -27,7 +33,17 @@ print "Problem 3 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 4 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+def add(x, y):
+    numbSum= x+y
+    return numbSum
+
+total= add(a,b)
+print total
+
+# You can identify a function if you see a variable
+# that has arguments that need to be passed to it
+# (anything with a name and parenthesis that take arguments)
+# For example: a(x, y), sum(a, b, c, d) would be functions
 
 
 ###
@@ -37,7 +53,10 @@ print "Problem 4 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 5 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+v={"a": a, "b": b}
+
+assert v=={"a":2, "b":3}
+
 
 ###
 ### Problem 6
@@ -46,7 +65,17 @@ print "Problem 5 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 6 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+v2={}
+v2["a"]=a
+v2["b"]=b
+
+assert v==v2
+
+# this method allows you to add new keys/values to the existing dictionary
+# without having to type out the whole dictionary again with the new key
+# For example: v={"a": a, "b": b} if i want to add c, I could just do
+# v["c"]= 4 instead of typing out the whole dictionary of
+# v={"a": a, "b": b, "c": 4} 
 
 ###
 ### Problem 7
@@ -55,11 +84,16 @@ print "Problem 6 solution follows:"
 # DO NOT CHANGE THE FOLLOWING LINE
 print "Problem 7 solution follows:"
 
-# ... write your code and comments here (and remove this line)
+for keys in v:
+    print keys
+    print v[keys]
+    
+# k is all the keys in the dictionary and dictionary["k"] is all the
+# values for the keys in the dictionary.
 
 ###
 ### Collaboration
 ###
 
-# ... List your collaborators and other sources of help here (websites, books, etc.),
-# ... as a comment (on a line starting with "#").
+# ... Andrew Loewen (loeand16)
+# ... 
